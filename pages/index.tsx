4851b548f1de4023fcdbf496352de32221b05957
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-05-27 15:09:03 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
+import { useEffect } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return {
-		    redirect: {
-		      permanent: false,
-		      destination: `${
-			`https://amzn.to/42aFW0T`
-		      }`,
-		    },
-		  };
-		}
+  useEffect(() => {
+    window.location.href = 'https://moviesing.com';
+  }, []);
 
-export default Home
+  return null;
+};
+
+export default Home;
