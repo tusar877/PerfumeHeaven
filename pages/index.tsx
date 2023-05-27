739793a5f0382c8a196ac58fba02d0263f7a5756
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-05-27 15:12:57 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.location.href = 'https://moviesing.com';
+    window.location.href = 'https://amzn.to/42aFW0T';
   }, []);
 
   return null;
