2d60254263d41e95e7673d827399091cc022d67d
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-05-27 10:39:00 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`https://amzn.to/42aFW0T`
 				}`,
 			},
 		};
